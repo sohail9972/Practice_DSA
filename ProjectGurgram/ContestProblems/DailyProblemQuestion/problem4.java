8eb6d3902fec880b077908d09f04b85ac4648281
sohail9972 <2018ece_sohailmaidargi@nie.ac.in>
2024-12-18 23:20:20 +0530

    adding the changes for the Day4

---
+++ ProjectGurgram/ContestProblems/DailyProblemQuestion/problem4.java
@@ -0,0 +1,26 @@
+package ProjectGurgram.ContestProblems.DailyProblemQuestion;
+
+public class problem4 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int[] finalPrices(int[] prices) {
+            int n = prices.length;
+            int[] res = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                res[i] = prices[i];
+
+                for (int j = i + 1; j < n; j++) {
+                    if (prices[j] <= prices[i]) {
+                        res[i] -= prices[j];
+                        break;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
