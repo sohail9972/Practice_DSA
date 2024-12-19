db6b036de3f1bac7f4994fa0c6139ec4a6a629c0
sohail9972 <2018ece_sohailmaidargi@nie.ac.in>
2024-12-19 22:10:22 +0530

    adding the Problem 5

---
+++ ProjectGurgram/ContestProblems/DailyProblemQuestion/problem5.java
@@ -0,0 +1,20 @@
+package ProjectGurgram.ContestProblems.DailyProblemQuestion;
+
+public class problem5 {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int maxChunksToSorted(int[] arr) {
+            int runningSum = 0, expectedSum = 0, chunks = 0;
+            for (int i = 0; i < arr.length; i++) {
+                runningSum += arr[i];
+                expectedSum += i;
+                if (runningSum == expectedSum) {
+                    chunks++;
+                }
+            }
+            return chunks;
+        }
+    }
+}
